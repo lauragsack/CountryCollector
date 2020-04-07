b1eaeaeb08705d80787b4b7a8b144004402c505d
lsack <lauragsack@gmail.com>
2020-04-07 14:44:27 -0700

    adds Country model

--- countrycollector/main_app/models.py
+++ countrycollector/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    continent = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
+    visited = models.BooleanField()
--- countrycollector/main_app/views.py
+++ countrycollector/main_app/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Country:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-countries = [
-  Country('Lolo', 'tabby', 'foul little demon', 3),
-  Country('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Country('Raven', 'black tripod', '3 legged cat', 4)
-]
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render (request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
