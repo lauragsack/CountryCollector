55b93a8b2477fb17b0cf9ccf65c01df13664ae4e
lsack <lauragsack@gmail.com>
2020-04-08 14:19:15 -0700

    fixes form checkbox

--- countrycollector/main_app/views.py
+++ countrycollector/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Country
+from .forms import CityForm
 # Create your views here.
 def home(request):
   return render (request, 'home.html')
@@ -13,4 +14,8 @@ def countries_index(request):
 
 def countries_detail(request, country_id):
     country = Country.objects.get(id = country_id)
-    return render(request, 'countries/detail.html', { 'country': country })
+    city_form = CityForm()
+    return render(request, 'countries/detail.html', { 
+        'country': country,
+        'city_form': city_form
+        })
