6a1941120401a16762fe6dd140402f74b20fbda7
lsack <lauragsack@gmail.com>
2020-04-08 14:52:47 -0700

    commits post view

--- countrycollector/main_app/urls.py
+++ countrycollector/main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name ='about'),
     path('countries/', views.countries_index, name='index'),
     path('countries/<int:country_id>/', views.countries_detail, name='detail'),
+    path('countries/<int:country_id>/add_city', views.add_city, name='add_city'),
 ]
--- countrycollector/main_app/views.py
+++ countrycollector/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Country
 from .forms import CityForm
 # Create your views here.
@@ -19,3 +19,11 @@ def countries_detail(request, country_id):
         'country': country,
         'city_form': city_form
         })
+
+def add_city(request, country_id):
+    form = CityForm(request.POST)
+    if form.is_valid():
+        new_city = form.save(commit=False)
+        new_city.country_id = country_id
+        new_city.save()
+    return redirect('detail', country_id=country_id)
